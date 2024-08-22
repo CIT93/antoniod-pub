39c350916a20ee65d194ad66e76613fc57b50c35
Antonio Dwyer <adwyer650@gmail.com>
2024-08-22 07:28:35 -0700

    Updated the code in main.js

--- w2/main.js
+++ w2/main.js
@@ -1,1 +1,25 @@
 console.log ("Hello CIT93")
+
+// How to calculate your footprint
+
+//1. Count the members of you household.
+const myHousemembers = 12;
+//2. Size of your home
+const myHomesize = 2;
+//3. Your food consumption
+const myFoodconsumption = 10;
+//4. Your water comsumption
+const myWater = 2;
+//5. Yearly Purchases.
+const myPurchases = 6;
+//6. Your waste.
+const myWaste = 30;
+//7. your recycle habbits.
+const myRecycle = 24;
+//8. You transportation.
+const myRide = 6;
+//9. My Total Carbon Foot print
+const myTotalcfp = myFoodconsumption+myHomesize+myHousemembers+myPurchases+myRecycle+myRide+myWaste+myWater
+
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Antonio's Carbon footprint is 94";
