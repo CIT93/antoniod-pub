a01814ec9bda2a4182782c51a8708747f9cbca62
Antonio Dwyer <adwyer650@gmail.com>
2024-11-07 04:13:50 -0800

    my code before looking at the solution

--- w13/main.js
+++ w13/main.js
@@ -1,20 +1,34 @@
 const FORM = document.getElementById("form");
 
 const startExercise = (exerciseType, number, time) => {
-    const output = document.getElementById("output");
-    output.textContent = `Based on your ${exerciseType}, your ${number} numbers of Reps, this is the amount of ${time} time to complete`;
+    return new Promise((resolve, reject) => {
+        
+        if ( number < 1 || number >30 ) {
+            reject(`ERROR: ${number} outside of range`);
+            return;
+        }
 
-    setTimeout(() => {
-        output.textContent = "Workout Complete";
-    }, time * 5000);
+        output.textContent = `Based on your ${exerciseType}, your ${number} numbers of Reps, this is the amount of ${time} time to complete`;
+
+        setTimeout(() => {
+            resolve("Workout Complete");
+        }, time * 5000);
+    });
 };
+
+const messageError = (error) => {
+    displayOutput(error, "h3")
+  }
+  
 
 FORM.addEventListener("submit", (e) => {
     e.preventDefault();
     const exerciseType = FORM.exerciseType.value;
     const Repsnumber = FORM.Repsnumber.value;
     const Time = FORM.Time.value;
-    startExercise(exerciseType, Repsnumber, Time);
+
+    startExercise(exerciseType, Repsnumber, Time)
+        .then(displayOutput)
+        .catch(messageError)
     FORM.reset();
 });
-
