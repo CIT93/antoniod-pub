9c04eeb9e410d75a1a793ad67dfc329e3325819c
Antonio Dwyer <adwyer650@gmail.com>
2025-01-27 08:27:19 -0800

    week 3

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,20 @@
-console.log("Week 1 -- Hello World!");
+console.log("Hello from inside the main.js file");
+
+// let myVar;
+const myVar = 42;
+const myVarType = typeof ( myVar )
+
+console.log ( "myVarType" + myVarType )
+console.log ( ` myVarType ${ myVarType } ` )
+
+
+
+function runNow() {
+  if (myVarType === "number") {
+    console.log(`will 11 this one run?`);
+  } else {
+    console.log(`will 13 this one run`);
+  }
+}
+
+runNow()
