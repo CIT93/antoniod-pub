92b728c3042433225510636ae88a80b73031a89d
Antonio Dwyer <adwyer650@gmail.com>
2025-02-20 07:48:53 -0800

    my attempt at the code before looking at others.

--- w6/main.js
+++ w6/main.js
@@ -38,16 +38,21 @@ function determineHouseHoldPts(numberInHousehold) {
 
 function displayOutObj(obj) {
   const output = document.getElementById("output");
+  const newH1 = document.createElement("h1");
+  newH1.textContent = `${obj.firstName} ${obj.lastName} has a `;
   const newH2 = document.createElement("h2");
   newH2.textContent = `Carbon Footprint total is ${obj.cfpTotal}`;
+  output.appendChild(newH1);
   output.appendChild(newH2);
 }
 
-function start(houseHoldMembers, houseSize) {
+function start(houseHoldMembers, houseSize, firstName, lastName) {
   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
   const houseSizePts = determineHouseSizePts(houseSize);
   const total = houseHoldPTS + houseSizePts;
   cfpData.push({
+    firstName: firstName,
+    lastName: lastName,
     houseHold: houseHoldMembers,
     houseSize: houseSize,
     houseHoldPts: houseHoldPTS,
@@ -57,15 +62,17 @@ function start(houseHoldMembers, houseSize) {
 }
 
 function displayOutput() {
-  for (obj of cfpData) {
-    console.log(obj);
+  for (const obj of cfpData) {
     const newP = document.createElement("p");
+    const newH1 = document.createElement("h1");
+    newH1.textContent = `${obj.firstName} ${obj.lastName} has a `;
     const newH2 = document.createElement("h2");
     newH2.textContent = `Carbon Footprint total is ${obj.cfpTotal}`;
     const newH3 = document.createElement("h3");
     newH3.textContent = `Based on number in Household and size of home`;
-    newP.textContent = `This number is based on the number of the household of ${obj.houseHold} (score: ${obj.houseHoldPts}`;
-    newP.textContent += `and a ${obj.houseSize} size of home (score ${obj.houseSizePts})`;
+    newP.textContent = `This number is based on the number of the household of ${obj.houseHold} (score: ${obj.houseHoldPts}) `;
+    newP.textContent += `and a ${obj.houseSize} size of home (score ${obj.houseSizePts}).`;
+    OUTPUT.appendChild(newH1);
     OUTPUT.appendChild(newH2);
     OUTPUT.appendChild(newH3);
     OUTPUT.appendChild(newP);
@@ -78,10 +85,8 @@ FORM.addEventListener("submit", function (e) {
   const lastName = FORM.lastname.value;
   const houseMembers = parseInt(FORM.householdmembers.value);
   const houseSize = FORM.housesize.value;
-  start(houseMembers, houseSize);
+  start(houseMembers, houseSize, firstName, lastName);
   OUTPUT.innerHTML = "";
   displayOutput();
   FORM.reset();
 });
-
-
