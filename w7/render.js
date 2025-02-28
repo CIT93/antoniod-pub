d7498121e27c2aec32422d710caa68f2bd9c45b7
Antonio Dwyer <adwyer650@gmail.com>
2025-02-28 02:16:26 -0800

    updated my code

--- w7/render.js
+++ w7/render.js
@@ -2,16 +2,23 @@ const TBL = document.getElementById("tab-data");
 
 
 function renderTblHeading() {
+    TBL.innerHTML = "";
     const table = document.createElement("table");
     const thead = document.createElement("thead");
     const tr = document.createElement("tr");
     const headingTextArr = [
       "Name",
-      "Household",
-      "HouseSize",
+      
       "Footprint",
-      "Actions",
+      
     ];
+    // //const headingTextArr = [
+    //   "Name",
+    //   "Household",
+    //   "HouseSize",
+    //   "Footprint",
+    //   "Actions",
+    // //];
     headingTextArr.forEach(function (text) {
       const th = document.createElement("th");
       th.textContent = text;
@@ -26,26 +33,32 @@ function renderTblHeading() {
   function renderTbl(data) {
     const table = renderTblHeading();
     const tbody = document.createElement("tbody");
-    const tr = document.createElement("tr");
-    const trTextArr = ["Antonio", 3, "Large", 20];
-    trTextArr.forEach(function (text) {
-      const td = document.createElement("td");
-      td.textContent = text;
-      tr.appendChild(td);
+    
+    //const trTextArr = ["Antonio", 3, "Large", 20];
+    data.forEach(function (obj) {
+      const tr = document.createElement("tr");
+      const tdName = document.createElement("td");
+      tdName.textContent = obj.firstName;
+      const tdTotal = document.createElement("td");
+      tdTotal.textContent = obj.cfpTotal;
+      tr.appendChild(tdName);
+      tr.appendChild(tdTotal);
+      tbody.appendChild(tr);
     });
-    const td = document.createElement("td");
-    const btnEdit = document.createElement("button");
-    btnEdit.textContent = "Edit";
-    const btnDelete = document.createElement("button");
-    btnDelete.textContent = "Delete";
-    td.appendChild(btnEdit);
-    td.appendChild(btnDelete);
-    tr.appendChild(td);
+    // const td = document.createElement("td");
+    // const btnEdit = document.createElement("button");
+    // btnEdit.textContent = "Edit";
+    // const btnDelete = document.createElement("button");
+    // btnDelete.textContent = "Delete";
+    // td.appendChild(btnEdit);
+    // td.appendChild(btnDelete);
+    // tr.appendChild(td);
   
-    tbody.appendChild(tr);
+    
     table.appendChild(tbody);
   
     TBL.appendChild(table);
+    
   }
 
   
