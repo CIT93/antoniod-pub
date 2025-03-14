5c8dcfc2db0b154c238a4f5010bd75cf5f0a6071
Antonio Dwyer <adwyer650@gmail.com>
2025-03-14 07:03:11 -0700

    updated code to solution

--- w9/main.js
+++ w9/main.js
@@ -3,6 +3,10 @@ import { determineHouseHoldPts, determineHouseSizePts } from "./cfp.js";
 import { FORM } from "./global.js";
 import { saveLS, cfpData } from "./storage.js";
 
+const firstNameEl = document.getElementById("firstName");
+const lastNameEl = document.getElementById("lastName");
+const submitEl = document.getElementById("submitError");
+
 function start(first, last, houseHoldMembers, houseSize) {
   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
   const houseSizePTS = determineHouseSizePts(houseSize);
@@ -20,18 +24,39 @@ function start(first, last, houseHoldMembers, houseSize) {
 
 renderTbl(cfpData);
 
+function validateField(event) {
+  const field = event.target.value;
+  const fieldId = event.target.id;
+  const fieldError = document.getElementById(`${fieldId}Error`);
+
+  if (field === "") {
+    fieldError.textContent = `${fieldId} is required`;
+    event.target.classList.add("invalid");
+  } else {
+    fieldError.textContent = "";
+    event.target.classList.remove("invalid");
+  }
+}
+
+//attach Blur listners
+firstNameEl.addEventListener("blur", validateField);
+lastNameEl.addEventListener("blur", validateField);
+
 FORM.addEventListener(`submit`, function (e) {
   e.preventDefault();
   const firstName = FORM.firstname.value;
   const lastName = FORM.lastname.value;
-  if(!firstName || lastName) {
-    alert("Please add First and Last name")
-    return
+  const firstNameIsValid = firstNameEl.value !== "";
+  const lastNameIsValid = lastNameEl.value !== "";
+  if (firstNameIsValid && lastNameIsValid) {
+    submitEl.textContent = "";
+    const houseMembers = parseInt(FORM.housem.value);
+    const houseSize = FORM.houses.value;
+    start(firstName, lastName, houseMembers, houseSize);
+    saveLS(cfpData);
+    renderTbl(cfpData);
+    FORM.reset();
+  } else {
+    submitEl.textContent = "Form requires first name and last name";
   }
-  const houseMembers = parseInt(FORM.housem.value);
-  const houseSize = FORM.houses.value;
-  start(firstName, lastName, houseMembers, houseSize);
-  saveLS(cfpData);
-  renderTbl(cfpData);
-  FORM.reset();
 });
